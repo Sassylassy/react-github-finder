20802faf44831d6c615e4f89e9bf9540c95ab0c7
Sarah Wimark <sarahwimark@gmail.com>
2019-06-05 16:49:51 +0200

    Adding functionality to search users

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,40 +1,32 @@
-import React, { Component } from "react";
+import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "blaj1"
-      },
-      {
-        id: "2",
-        login: "blaj2"
-      },
-      {
-        id: "3",
-        login: "blaj3"
-      }
-    ]
-  };
-
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
-    );
+    )
   }
 }
 
 const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3,1fr)',
-    gridGap: '1rem'
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3,1fr)',
+  gridGap: '1rem'
 
 }
 
-export default Users;
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+}
+
+export default Users
