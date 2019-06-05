7f6fe5ee9b752efbefd0710097a8df8ff216329e
Sarah Wimark <sarahwimark@gmail.com>
2019-06-05 15:27:02 +0200

    Adding navbar and useritem

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class UserItem extends Component {
+  state = {
+      id: 'id',
+      login: 'mojombo',
+      avatar_url: '',
+      html_url: ''
+  }
+
+
+  render () {
+    const {login, avatar_url, html_url} = this.state
+    
+    return (
+      <div className='card text-center'>
+          <img src={avatar_url} className="round-img" style={{width: '60px'}} alt=""/>
+          <h3>{login}</h3>
+          <div>
+              <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+          </div>
+      </div>
+    )
+  }
+}
+
+export default UserItem
